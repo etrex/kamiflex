5c8d2eeefcce0442671d5298b4bd4d9fbb2a2e0b
etrex kuo <et284vu065k3@gmail.com>
2019-10-07 11:30:12 +0800

    add index

---
+++ example/box_with_resource.rb
@@ -0,0 +1,25 @@
+require_relative '../lib/kamiflex'
+
+@resources = [
+  {
+    name: "ruby"
+  },
+  {
+    name: "rails"
+  },
+  {
+    name: "kamiflex"
+  }
+]
+
+json = Kamiflex.build(self) do
+  bubble do
+    body do
+      vertical_box @resources do |resource|
+        text "#{resource[:name]}"
+      end
+    end
+  end
+end
+
+puts json
--- lib/kamiflex/core.rb
+++ lib/kamiflex/core.rb
@@ -17,8 +17,8 @@ module Kamiflex
     end
 
     def bubbles(resources)
-      resources.each do |resource|
-        attributes, _contents = flex_scope{ yield resource }
+      resources.each_with_index do |resource|
+        attributes, _contents = flex_scope{ yield(resource, index) }
         @flex_contents << {
           type: "bubble",
         }.merge(attributes.slice(:size, :direction, :header, :hero, :body, :footer, :style, :action))
@@ -70,8 +70,8 @@ module Kamiflex
 
     # container
     def horizontal_box(resources = [nil], **params)
-      resources.each do |resource|
-        _attributes, contents = flex_scope{ yield resource }
+      resources.each_with_index do |resource, index|
+        _attributes, contents = flex_scope{ yield(resource, index) }
         @flex_contents << {
           type: "box",
           layout: "horizontal",
